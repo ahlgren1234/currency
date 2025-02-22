8501ebe17aba1619d3d9572a607004862b6d9668
Peter Ahlgren <ahlgren1234@gmail.com>
2025-02-22 10:11:55 +0100

    Added flags

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -33,29 +33,29 @@ const API_KEY = '062578696d00dab96b6c2855'; // You'll need to replace this with 
 const API_URL = 'https://api.exchangerate-api.com/v4/latest/USD';
 
 // Currency to country code mapping
-const currencyToCountry: { [key: string]: string } = {
-  USD: 'us',
-  EUR: 'eu',
-  GBP: 'gb',
-  JPY: 'jp',
-  AUD: 'au',
-  CAD: 'ca',
-  CHF: 'ch',
-  CNY: 'cn',
-  SEK: 'se',
-  NZD: 'nz',
-  NOK: 'no',
-  SGD: 'sg',
-  KRW: 'kr',
-  TRY: 'tr',
-  INR: 'in',
-  BRL: 'br',
-  ZAR: 'za',
-  HKD: 'hk',
-  MXN: 'mx',
-  RUB: 'ru',
-  THB: 'th',
-  // Add more mappings as needed
+const currencyToCountry: Record<string, string> = {
+  USD: 'us', AED: 'ae', AFN: 'af', ALL: 'al', AMD: 'am', ANG: 'cw', AOA: 'ao',
+  ARS: 'ar', AUD: 'au', AWG: 'aw', AZN: 'az', BAM: 'ba', BBD: 'bb', BDT: 'bd',
+  BGN: 'bg', BHD: 'bh', BIF: 'bi', BMD: 'bm', BND: 'bn', BOB: 'bo', BRL: 'br',
+  BSD: 'bs', BWP: 'bw', BYN: 'by', BZD: 'bz', CAD: 'ca', CDF: 'cd', CHF: 'ch',
+  CLP: 'cl', CNY: 'cn', COP: 'co', CRC: 'cr', CUP: 'cu', CVE: 'cv', CZK: 'cz',
+  DJF: 'dj', DKK: 'dk', DOP: 'do', DZD: 'dz', EGP: 'eg', ERN: 'er', ETB: 'et',
+  EUR: 'eu', FJD: 'fj', FKP: 'fk', GBP: 'gb', GEL: 'ge', GHS: 'gh', GIP: 'gi',
+  GMD: 'gm', GNF: 'gn', GTQ: 'gt', GYD: 'gy', HKD: 'hk', HNL: 'hn', HRK: 'hr',
+  HTG: 'ht', HUF: 'hu', IDR: 'id', ILS: 'il', INR: 'in', IQD: 'iq', IRR: 'ir',
+  ISK: 'is', JMD: 'jm', JOD: 'jo', JPY: 'jp', KES: 'ke', KGS: 'kg', KHR: 'kh',
+  KMF: 'km', KRW: 'kr', KWD: 'kw', KYD: 'ky', KZT: 'kz', LAK: 'la', LBP: 'lb',
+  LKR: 'lk', LRD: 'lr', LSL: 'ls', LYD: 'ly', MAD: 'ma', MDL: 'md', MGA: 'mg',
+  MKD: 'mk', MMK: 'mm', MNT: 'mn', MOP: 'mo', MRU: 'mr', MUR: 'mu', MVR: 'mv',
+  MWK: 'mw', MXN: 'mx', MYR: 'my', MZN: 'mz', NAD: 'na', NGN: 'ng', NIO: 'ni',
+  NOK: 'no', NPR: 'np', NZD: 'nz', OMR: 'om', PAB: 'pa', PEN: 'pe', PGK: 'pg',
+  PHP: 'ph', PKR: 'pk', PLN: 'pl', PYG: 'py', QAR: 'qa', RON: 'ro', RSD: 'rs',
+  RUB: 'ru', RWF: 'rw', SAR: 'sa', SBD: 'sb', SCR: 'sc', SDG: 'sd', SEK: 'se',
+  SGD: 'sg', SHP: 'sh', SLE: 'sl', SOS: 'so', SRD: 'sr', SSP: 'ss', STN: 'st',
+  SYP: 'sy', SZL: 'sz', THB: 'th', TJS: 'tj', TMT: 'tm', TND: 'tn', TOP: 'to',
+  TRY: 'tr', TTD: 'tt', TWD: 'tw', TZS: 'tz', UAH: 'ua', UGX: 'ug', UYU: 'uy',
+  UZS: 'uz', VES: 've', VND: 'vn', VUV: 'vu', WST: 'ws', XAF: 'cm', XCD: 'ag',
+  XOF: 'sn', XPF: 'pf', YER: 'ye', ZAR: 'za', ZMW: 'zm', ZWL: 'zw'
 };
 
 interface ExchangeRates {
